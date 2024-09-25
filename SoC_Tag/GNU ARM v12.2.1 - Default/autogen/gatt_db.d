d9d430083ac95dabe7080a163a99bea36b83f87b
Nino Dragicevic <nino.dragicevic@probotica.hr>
2024-09-25 13:57:32 +0200

    deleted duplicates

--- SoC_Tag/GNU ARM v12.2.1 - Default/autogen/gatt_db.d
+++
@@ -1,5 +0,0 @@
-autogen/gatt_db.o: ../autogen/gatt_db.c \
- /home/nino/SimplicityStudio/SDKs/gecko_sdk//platform/common/toolchain/inc/sl_gcc_preinclude.h \
- /home/nino/SimplicityStudio/SDKs/gecko_sdk//protocol/bluetooth/inc/sli_bt_gattdb_def.h
-/home/nino/SimplicityStudio/SDKs/gecko_sdk//platform/common/toolchain/inc/sl_gcc_preinclude.h:
-/home/nino/SimplicityStudio/SDKs/gecko_sdk//protocol/bluetooth/inc/sli_bt_gattdb_def.h:
--- SoC_Tag/GNU ARM v12.2.1 - Default/gecko_sdk_4.4.1/platform/common/src/sl_slist.d
+++
@@ -1,8 +0,0 @@
-gecko_sdk_4.4.1/platform/common/src/sl_slist.o: \
- /home/nino/SimplicityStudio/SDKs/gecko_sdk/platform/common/src/sl_slist.c \
- /home/nino/SimplicityStudio/SDKs/gecko_sdk//platform/common/toolchain/inc/sl_gcc_preinclude.h \
- /home/nino/SimplicityStudio/SDKs/gecko_sdk//platform/common/inc/sl_assert.h \
- /home/nino/SimplicityStudio/SDKs/gecko_sdk//platform/common/inc/sl_slist.h
-/home/nino/SimplicityStudio/SDKs/gecko_sdk//platform/common/toolchain/inc/sl_gcc_preinclude.h:
-/home/nino/SimplicityStudio/SDKs/gecko_sdk//platform/common/inc/sl_assert.h:
-/home/nino/SimplicityStudio/SDKs/gecko_sdk//platform/common/inc/sl_slist.h:
